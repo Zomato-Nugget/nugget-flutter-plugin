20071017058f39aab778e382864ee8f9cea9ffaa
Rajesh Budhiraja <rajesh.budhiraja@zomato.com>
2025-06-02 17:05:55 +0530

    Resolved conflicts

--- nugget_flutter_plugin/lib/interface/nugget_auth_info.dart
+++ nugget_flutter_plugin/lib/interface/nugget_auth_info.dart
@@ -1,12 +1,7 @@
 abstract class NuggetAuthInfo {
-<<<<<<< HEAD
     String get accessToken;
     String get requestId;
     int get httpStatusCode;
     /// Serializes this NuggetAuthInfo instance to a JSON map.
     Map<String, dynamic> toJson();
-=======
-    int get clientID;
-    String get accessToken;
->>>>>>> 4ade6faafdd328c2b0d4550e89e7971b5f141c7a
 } 
--- nugget_flutter_plugin/lib/interface/nugget_auth_provider_delegate.dart
+++ nugget_flutter_plugin/lib/interface/nugget_auth_provider_delegate.dart
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import 'package:nugget_flutter_plugin/interface/nugget_auth_info.dart';
 
 abstract class NuggetAuthProviderDelegate {
     Future<NuggetAuthInfo?> requireAuthInfo();
     Future<NuggetAuthInfo?> refreshAuthInfo();
     Future<String?> fetchAccessTokenFromClient();
-=======
-import 'nugget_auth_user_info.dart';
-
-abstract class NuggetAuthProviderDelegate {
-    Future<NuggetAuthUserInfo?> requireAuthInfo();
-    Future<NuggetAuthUserInfo?> refreshAuthInfo();
->>>>>>> 4ade6faafdd328c2b0d4550e89e7971b5f141c7a
 } 
